39a18b065103aa783c579171709c522f262b9959
KaustubhKarnam <kaustubhkarnam@gmail.com>
2020-11-23 01:08:09 +0100

    Task 3 part 1; circle filtering

--- Assignment.py
+++ Assignment.py
@@ -45,7 +45,7 @@ def x_y_coordinates_array (size_of_matrix,radius_of_element):
 
 x,y = x_y_coordinates_array(n,r) # Funtion call to get x,y values as numpy array.
 
-#######################################################################
+""" #######################################################################
 # Code to plot the hexagonal n*n grid of elements # 
 
 fig, ax = plt.subplots()
@@ -58,10 +58,60 @@ for i in range(0,n*n):
 ax.set_xlim(-r,n)
 ax.set_ylim(-r,n)
 plt.show()
-#######################################################################
+#######################################################################  """
 
 #######################################################################
 #######################################################################
 # Task 1 & 2 Solution End
+#######################################################################
+#######################################################################
+
+#######################################################################
+#######################################################################
+# Task 3 Solution Start
+#######################################################################
+#######################################################################
+
+def isInside(circle_x, circle_y, rad, x, y,n): 
+    count = []
+    filtered_funtion_x = []
+    filtered_funtion_y = []
+    # Compare radius of circle 
+    # with distance of its center 
+    # from given point 
+    for i in range (0,n*n):
+        if ((x[i] - circle_x) * (x[i] - circle_x) + 
+            (y[i] - circle_y) * (y[i] - circle_y) <= rad * rad): 
+            count.append(i)
+            filtered_funtion_x.append(x[i])
+            filtered_funtion_y.append(y[i])
+    return (np.array(filtered_funtion_x),np.array(filtered_funtion_y),np.array(count))       
+         
+  
+# calling 
+circle_x = 10 
+circle_y = 10  
+rad = 5
+filtered_x, filtered_y, count = isInside(circle_x, circle_y, rad, x, y, n)
+
+#######################################################################
+# Code to plot the hexagonal n*n grid of elements # 
+
+fig, ax = plt.subplots()
+for i in range(0,len(filtered_y)):
+    value = count[i]
+    circles = plt.Circle((filtered_x[i], filtered_y[i]), r, color='black')
+    ax.add_artist(circles) 
+    plt.annotate(value, (filtered_x[i], filtered_y[i]), color = 'white')
+
+
+ax.set_xlim(-r,n)
+ax.set_ylim(-r,n)
+plt.show()
+#######################################################################
+
+#######################################################################
+#######################################################################
+# Task 3 Solution End
 #######################################################################
 #######################################################################
